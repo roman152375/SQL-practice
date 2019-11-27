965c54c01ba61761d5d0894e4ff57f0fdd04d5fb
Andres Mendoza <felipem1145@gmail.com>
2019-11-26 19:40:19 -0500

    Create ratul2ndclass.sql

---
+++ ratul2ndclass.sql
@@ -0,0 +1,23 @@
+--CREATE SCHEMA Examples;
+
+--GO
+
+
+
+CREATE TABLE Examples.Widget (
+
+ WidgetCode Varchar (10) NOT NULL CONSTRAINT PKWidget PRIMARY KEY,
+
+ WidgetName varchar (100) NULL,
+
+ WidgetPrice varchar (10) NULL
+
+);
+
+
+
+select * from examples.widget
+
+
+
+DROP TABLE Examples.Widget;
